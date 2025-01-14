41bb10397adb425e3669674345cc91da16ae4b90
aanicolella <ally.nicolella@gmail.com>
2025-01-14 10:13:50 -0800

    update parsers tests

--- example.py
+++ example.py
@@ -36,7 +36,6 @@ def main():
         print(record[0])
         print(transcribe(record[1],reverse=True))
 
-
 """
 When executing a python script from the command line there will
 always be a hidden variable `__name__` set to the value `__main__`.
--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -23,7 +23,7 @@ def test_freebie_parser_2():
     assert 1 != 2
 
         
-def test_FastaParser():
+def test_FastaParser_goodFile():
     """
     Write your unit test for your FastaParser class here. You should generate
     an instance of your FastaParser class and assert that it properly reads in
@@ -33,28 +33,54 @@ def test_FastaParser():
     files that are blank or corrupted in some way. Two example Fasta files are
     provided in /tests/bad.fa and /tests/empty.fa
     """
-    pass
-
+    testFasta = FastaParser('data/test.fa')
+    numSeq = 0
+    for record in testFasta:
+        numSeq+=1 # incriment counter for num seq test
+        # Check that values are assigned to both record slots based on expected formating:
+        assert record[0].startswith("seq") and record[1].startswith(tuple(["A","T","C","G"]))
+    # Check that number of records is the same as expected
+    assert numSeq==100
 
 def test_FastaFormat():
     """
     Test to make sure that a fasta file is being read in if a fastq file is
     read, the first item is None
     """
-    pass
+    # Check that fasta parser behaves as expected when used to read in fastq file
+    fastq_fastaParse = FastaParser('data/test.fq')
+    out = []
+    for record in fastq_fastaParse:
+        # extract records for easier checking
+        out.append(record[0])
+    assert len(out) != 100 # length will be longer than expected (100) if using fasta parsing rules for fastq format
+    # check that first element is None as specified in test prompt
+        ## not a generalized test because many of the first elements are actually not None, likely because 
+        ## quality scores sometimes include the ">" character
+    assert out[0] == None 
 
-
-def test_FastqParser():
+def test_FastqParser_goodFile():
     """
     Write your unit test for your FastqParser class here. You should generate
     an instance of your FastqParser class and assert that it properly reads 
     in the example Fastq File.
     """
-    pass
+    testFastq = FastqParser('data/test.fq')
+    numSeq = 0
+    for record in testFastq:
+        numSeq+=1 # incriment counter for num seq test
+        # Check that values are assigned to both record slots based on expected formating:
+        assert record[0].startswith("seq") and record[1].startswith(tuple(["A","T","C","G"])) and record[2]!=None
+    # Check that number of records is the same as expected
+    assert numSeq==100
 
 def test_FastqFormat():
     """
     Test to make sure fastq file is being read in. If this is a fasta file, the
     first line is None
     """
-    pass
+    # Check that fastq parser behaves as expected when used to read in fasta file
+    fasta_fastqParse = FastqParser('data/test.fa')
+    for record in fasta_fastqParse:
+        # check that first element is None as specified in test prompt--generalizable in this case
+        assert record[0] == None 
